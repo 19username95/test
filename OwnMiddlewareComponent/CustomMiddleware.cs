5f883af8cb6d536759f0f52abcb2e7774a2703a4
Dev1 <19kazakovayana95@icloud.com>
2022-05-08 18:52:40 +0200

    init commit

---
+++ OwnMiddlewareComponent/CustomMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace OwnMiddlewareComponent
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string method = context.Request.Method;
+            if (method == "GET")
+            {
+                context.Response.StatusCode = 200;
+                await context.Response.WriteAsync("GET!");
+            }
+            else
+            {
+                await next.Invoke(context);
+            }
+        }
+    }
+}
